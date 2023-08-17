f69b190afc2c578ae6550e69e0ead35aed4256e6
iwashi-yamashio <139742818+iwashi-yamashio@users.noreply.github.com>
2023-08-17 22:55:11 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let userWeight;
 let userHeight;
+let userBmi;
 
 //値の代入
 userWeight = 68;
 userHeight = 1.7
+userBmi = (userWeight / (userHeight * userHeight));
 
 // コンソールへの出力
-console.log(userWeight / (userHeight * userHeight));
+console.log(userBmi);
